d7e0cf0a37c86f9ae5d4af2bbbd7e0442eca8112
Matthew McCormack <mmccormack1182@gmail.com>
2019-05-02 14:20:16 -0400

    refactored tax_year views with table partials

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -3,4 +3,21 @@ class EmployersController < ApplicationController
   def edit
     @employer = Employer.find_by(id: params[:id])
   end
+
+  def update
+    @employer = Employer.find_by(id: params[:id])
+    @employer.update(update_params)
+    if @employer.save
+      tax_year = TaxYear.find_by(id: session[:current_tax_year_id])
+      redirect_to tax_year_path(tax_year)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def update_params
+    params.require(:employer).permit(:name, :tax_form)
+  end
 end
--- app/models/employer.rb
+++ app/models/employer.rb
@@ -2,8 +2,14 @@ class Employer < ApplicationRecord
   belongs_to :user
   has_many :checks
   has_many :tax_years, through: :checks
-  validates :id, uniqueness: true
+  scope :tax_form, -> { where(tax_form: true)}
 
+  def self.no_form
+    where(tax_form: false)
+  end
 
-  
+  def self.current_tax_year
+    TaxYear.find_by(id: session[:current_tax_year_id])
+  end
+
 end
