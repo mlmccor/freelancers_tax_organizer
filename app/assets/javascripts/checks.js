0c5ef023cca80a59fb45f20b59ef1d3aa1534691
Matthew McCormack <mmccormack1182@gmail.com>
2019-07-12 14:32:01 -0400

    fixed check links to show on main page

--- app/assets/javascripts/checks.js
+++ app/assets/javascripts/checks.js
@@ -17,7 +17,7 @@ class Check {
     let newDate = document.createElement('TD')
     newDate.innerHTML = `${date.toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit' })}`
     let name = document.createElement('TD')
-    name.innerHTML = `<a href="#" class= 'check' id=${this.id}>${this.name}</a>`
+    name.innerHTML = `<a href="#" onclick="loadCheckResults(); return false" class='check' id=${this.id}>${this.name}</a>`
     let amount = document.createElement('TD')
     amount.innerHTML = `$${this.amount}`
     let mileage = document.createElement('TD')
@@ -42,7 +42,7 @@ class Check {
     let newDate = document.createElement('TD')
     newDate.innerHTML = `${date.toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit' })}`
     let name = document.createElement('TD')
-    name.innerHTML = `<a href="#" onclick= displayCheck() id= check-${this.id}>${this.name}</a>`
+    name.innerHTML = `<a href="#" onclick= "loadCheckResults(); return false" class='check' id=${this.id}>${this.name}</a>`
     let employer = document.createElement('TD')
     employer.innerHTML = `<a href="#" id= employer->${this.employer}</a>`
     let amount = document.createElement('TD')
@@ -135,5 +135,27 @@ function addCheckListeners() {
         edit.innerHTML = `<a href="/checks/${check.id}/edit">Edit Check</a>`
       })
     })
+  })
+}
+
+function loadCheckResults() {
+  debugger
+  document.querySelector('.newForm').style.display= "none"
+  document.querySelector('#checkResult').style.display= "inline"
+  let id = $('.id').data('temp')
+  let answer= fetch(`/tax_years/${id}/checks/${event.target.id}.json`)
+  .then(response => response.json())
+  .then(json => {
+    debugger
+    check = new Check(json)
+    $('#checkName').text(`Name: ${check['name']}`)
+    let date = new Date(check['check_date'])
+    $('#checkDate').text(`Date: ${date.toDateString()}`)
+    $('#checkAmount').text(`Amount: $${check['amount']}`)
+    $('#checkMileage').text(`Mileage: ${check['mileage']}`)
+    $('#checkEmployer').text(`Employer: ${check['employer']}`)
+    $('#checkDescription').text(`Description: ${check['description']}`)
+    let edit = document.querySelector('#checkEditLink')
+    edit.innerHTML = `<a href="/checks/${check.id}/edit">Edit Check</a>`
   })
 }
